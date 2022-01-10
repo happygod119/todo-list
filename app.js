214508c24ef6b2e88df20a65de6d048bcaa9e450
happygod119 <happ0983493655@gmail.com>
2022-01-10 19:05:44 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser"); // 引用 body-parser
+
 const Todo = require("./models/todo"); // 載入 Todo model
 
 const exphbs = require("express-handlebars"); //載入handlebars
@@ -24,6 +26,9 @@ db.once("open", () => {
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // 設定首頁路由
 app.get("/", (req, res) => {
   //- 拿到全部的 Todo 資料
@@ -31,6 +36,26 @@ app.get("/", (req, res) => {
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
     .catch((error) => console.error(error)); // 錯誤處理
+});
+
+// 設定 new 路由
+app.get("/todos/new", (req, res) => {
+  return res.render("new");
+});
+
+app.post("/todos", (req, res) => {
+  const name = req.body.name; // 從 req.body 拿出表單裡的 name 資料
+  //- 直接操作Todo
+  return Todo.create({ name }) // 存入資料庫
+    .then(() => res.redirect("/")) // 新增完成後導回首頁
+    .catch((error) => console.log(error));
+
+  //- 先產生物件實例，再存入Todo
+  // const todo = new Todo({ name })
+  // return todo
+  //   .save()
+  //   .then(() => res.redirect("/"))
+  //   .catch((error) => console.log(error));
 });
 
 // 設定 port 3000
