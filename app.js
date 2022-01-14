a8d661a9f0e67b5ab25a373290d8bc7020864fba
happygod119 <happ0983493655@gmail.com>
2022-01-14 17:09:24 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get("/", (req, res) => {
   //- 拿到全部的 Todo 資料
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ name: "asc" }) //排序 asc-正序 & desc-反序
     .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
     .catch((error) => console.error(error)); // 錯誤處理
 });
@@ -86,7 +87,7 @@ app.post("/todos/:id/edit", (req, res) => {
   return Todo.findById(id)
     .then((todo) => {
       todo.name = name;
-      todo.isDone = isDone === 'on'
+      todo.isDone = isDone === "on";
       // if (isDone === "on") {
       //   todo.isDone = true;
       // } else {
