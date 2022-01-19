cc06561a0d3ade193f9beeca04485a80136a8488
happygod119 <happ0983493655@gmail.com>
2022-01-19 20:18:53 +0800

    feat: modify detail page ui

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express"); // 引用 Express 與 Express 路由器
 
-const router = express.Router() // 準備引入路由模組
+const router = express.Router(); // 準備引入路由模組
 
-const home = require('./modules/home') // 引入 home 模組程式碼
-const todos = require('./modules/todos')
+const home = require("./modules/home"); // 引入 home 模組程式碼
+const todos = require("./modules/todos");
 
-router.use('/', home)
-router.use('/todos', todos)
+router.use("/", home);
+router.use("/todos", todos);
 
-module.exports = router // 匯出路由器
+module.exports = router; // 匯出路由器
