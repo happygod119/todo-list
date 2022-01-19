39995df367fb78136677fd1e7d10f60bf73d0085
happygod119 <happ0983493655@gmail.com>
2022-01-19 21:47:07 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require("express");
 const app = express();
+const PORT =process.env.PORT || 3000
 
 const bodyParser = require("body-parser"); // 引用 body-parser
 
@@ -31,6 +32,6 @@ app.use(routes);
 
 
 // 設定 port 3000
-app.listen(3000, () => {
-  console.log("App is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
 });
