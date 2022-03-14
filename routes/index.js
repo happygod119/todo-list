0bd0f7297e95171c44968d95eda84cffb14ec468
happygod119 <happ0983493655@gmail.com>
2022-03-14 20:11:29 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router(); // 準備引入路由模組
 
 const home = require("./modules/home"); // 引入 home 模組程式碼
 const todos = require("./modules/todos");
+const users = require("./modules/users"); 
 
 router.use("/", home);
 router.use("/todos", todos);
+router.use("/users", users); 
 
 module.exports = router; // 匯出路由器
