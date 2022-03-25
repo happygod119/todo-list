34005c9caf320ab64bb89a5ca74b9d413a2c9a58
happygod119 <happ0983493655@gmail.com>
2022-03-25 19:28:15 +0800

    feat: add relations between User and Todo models

--- models/todo.js
+++ models/todo.js
@@ -10,6 +10,13 @@ const todoSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  userId: {
+    // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    index: true,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,8 +5,8 @@ const router = express.Router(); // 準備引入路由模組
 const Todo = require("../../models/todo");
 
 router.get("/", (req, res) => {
-  //- 拿到全部的 Todo 資料
-  Todo.find() // 取出 Todo model 裡的所有資料
+  const userId = req.user._id; // 變數設定
+  Todo.find({ userId }) // 加入查詢條件
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort({ name: "asc" }) //排序 asc-正序 & desc-反序
     .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
